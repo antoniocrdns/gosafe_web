98b47df0ba12122fee6d1186acce957c42206ad1
MiriamAlvarad0 <alvaradoherasmiriamselena@gmail.com>
2025-02-19 17:19:41 -0700

    Front end Homapage.jsx y Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import logo from "../assets/gosafe_logo.png"; // Asegúrate de que la imagen esté en la carpeta correcta
 
 function Login() {
     const navigate = useNavigate();
 
     const handleLogin = () => {
-        navigate("/dashboard")
+        navigate("/dashboard");
     };
 
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={handleLogin}>Iniciar sesión</button>
+        <div className="login-container">
+            <img src={logo} alt="Logo" className="login-logo" />
+            <div className="login-form">
+                <h2>Iniciar sesión</h2>
+                <input type="email" placeholder="Correo electrónico" className="login-input" />
+                <input type="password" placeholder="Contraseña" className="login-input" />
+                <button onClick={handleLogin} className="login-button">Iniciar sesión</button>
+            </div>
         </div>
     );
 }
